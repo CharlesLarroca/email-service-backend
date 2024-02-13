b2043abd89a169a69964b6f1d199a84d75d6e616
Charles Larroca <larroca_1000@hotmail.com>
2024-02-12 23:38:57 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
-const fastify = require("fastify");
-const EmailRoutes = require("./routes/email");
-const cors = require("@fastify/cors");
-const formBody = require("@fastify/formbody");
+const fastify = require('fastify')
+const server= fastify()
 
-const server = fastify();
-
-const corsOptions = {
-    credentials: true,
-    origin:  /localhost\:5173/,
-}
-server.register(cors, corsOptions);
-server.register(formBody);
-
-server.register(EmailRoutes);
+server.get('/', () => 'Hello World')
 
 server.listen({
-    port: process.env.PORT || 3200
-})
+    port: process.env.PORT || 3000
+}, () => console.log('http://localhost:3000'))
